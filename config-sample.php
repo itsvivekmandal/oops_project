fc36f129b7eb58e677f1a56dcf5bb34eaf0e9693
Mr. Zero <vivek248.vm@gmail.com>
2023-12-26 00:10:32 +0530

    config sample file created

---
+++ config-sample.php
@@ -0,0 +1,10 @@
+<?php
+// Define Database details
+define('DB_HOST', 'localhost');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+
+// Define URL
+define('ROOT_PATH', '/oops_project');
+define('ROOT_URL', 'http://localhost/oops_project');
